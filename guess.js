69ab5cc484dea8bb9148574af2d7988faab2ce1c
kaidecoder <131601358+kaidecoder@users.noreply.github.com>
2023-11-26 14:27:57 -0500

    working on the logic

--- guess.js
+++ guess.js
@@ -1,29 +1,29 @@
-const p= document.querySelector("p")
-const alertBox = document.querySelector(".alert-box")
+const p = document.querySelector("p");
+const alertBox = document.querySelector(".alert-box");
 
-p.innerText = "Welcome to The Guessing Game"
-let yourNumber
-let computerGuess
+p.innerText = "Welcome to The Guessing Game";
+let yourNumber;
+let computerGuess;
 
-computerGuess = Math.floor(Math.random() * 100 + 1)
-console.log(computerGuess)
-while (yourNumber == null || yourNumber == "") {
-    yourNumber = Number(
-      prompt(
-        "Please enter a number:  ",
-      ),
-    );
+console.log(computerGuess);
+
+while (yourNumber == null || yourNumber == "" && yourNumber !== computerGuess) {
+    yourNumber = Number(prompt("Please enter a number:  "));
     continue;
-    
+}
+
+computerGuess = Math.floor(Math.random() * 100 + 1);
+
+while (yourNumber !== computerGuess) {
+  if (yourNumber > computerGuess) {
+    alertBox.classList.add("error");
+    p.innerText = "Your number is too large";
+  } else if (yourNumber < computerGuess) {
+    alertBox.classList.add("warning");
+    p.innerText = "Your number is too small";
+  } else {
+    alertBox.classList.add("success");
+    p.innerText = "SUCCESS";
   }
-  
-if(yourNumber > computerGuess){
-    alertBox.classList.add("error")
-    p.innerText = "Your number is too large"
-}else if(yourNumber < computerGuess){
-    alertBox.classList.add("warning")
-    p.innerText = "Your number is too small"
-}else{
-    alertBox.classList.add("success")
-    p.innerText = "SUCCESS"
+ 
 }
