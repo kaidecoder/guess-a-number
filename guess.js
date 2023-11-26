402bdd476fb3af35dc706a87f765389dd2cb9f97
kaidecoder <131601358+kaidecoder@users.noreply.github.com>
2023-11-26 17:53:03 -0500

    added a button to control when the game starts.  can only use the success box, as the alert box has to be closed to see the comment.

--- guess.js
+++ guess.js
@@ -12,17 +12,13 @@ function guessANumber(){
   
   yourNumber = Number(prompt("Please enter a number:  "));
   if (yourNumber > computerGuess) {
-    alertBox.classList.add("error");
-    p.innerText = "Your number is too large";
     console.log("too large");
   } else if (yourNumber < computerGuess) {
-    alertBox.classList.add("warning");
-    p.innerText = "Your number is too small";
     console.log("too small");
   } else {
     alertBox.classList.add("success");
     p.innerText = "SUCCESS";
-    console.log("success");
+    // console.log("success");
     let play = prompt("Would you like to play again? ")
   if(play !== "yes"){
     break
