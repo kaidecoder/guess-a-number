c63a6f723f2c766c6140e49c42ef9b854eba3c64
kaidecoder <131601358+kaidecoder@users.noreply.github.com>
2023-11-27 03:52:17 -0500

    use the debugger to follow the code to find where to generate a new number

--- guess.js
+++ guess.js
@@ -1,34 +1,47 @@
 const p = document.querySelector("p");
 const alertBox = document.querySelector(".alert-box");
-
+let text
 let yourNumber;
 let computerGuess
 computerGuess = Math.floor(Math.random() * 100 + 1);
   console.log(computerGuess);
 let endOfFile = false;
-function guessANumber(){
-  
+async function guessANumber(){
   while (!endOfFile) {
-  
+    // NOTE: NEED TO GENERATE A NEW NUMBER
   yourNumber = Number(prompt("Please enter a number:  "));
   if (yourNumber > computerGuess) {
     console.log("too large");
+    text = "too large"
   } else if (yourNumber < computerGuess) {
     console.log("too small");
+    text = "too small"
   } else {
     alertBox.classList.add("success");
-    p.innerText = "SUCCESS";
+    text = "SUCCESS";
     // console.log("success");
     let play = prompt("Would you like to play again? ")
   if(play !== "yes"){
+
     break
   }
+  
   continue
   }
   
   continue;
 }
 }
+
+
+const myPromise = new Promise((resolve, reject)=>{
+  if(guessANumber){
+    resolve('There is a value')
+  }else{
+    reject("there is no number")
+  }
+})
+myPromise.then(text)
 
 
 
