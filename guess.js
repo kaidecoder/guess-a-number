64dc3d97ef6ac30df1e2b09581bf2f7ca7604852
kaidecoder <131601358+kaidecoder@users.noreply.github.com>
2023-11-27 04:46:47 -0500

    how is the console stopping the blocking code?

--- guess.js
+++ guess.js
@@ -1,47 +1,58 @@
-const p = document.querySelector("p");
+let p = document.querySelector("p");
 const alertBox = document.querySelector(".alert-box");
-let text
+let text;
 let yourNumber;
-let computerGuess
+let computerGuess;
 computerGuess = Math.floor(Math.random() * 100 + 1);
-  console.log(computerGuess);
+console.log(computerGuess);
+
 let endOfFile = false;
-async function guessANumber(){
-  while (!endOfFile) {
-    // NOTE: NEED TO GENERATE A NEW NUMBER
-  yourNumber = Number(prompt("Please enter a number:  "));
-  if (yourNumber > computerGuess) {
-    console.log("too large");
-    text = "too large"
-  } else if (yourNumber < computerGuess) {
-    console.log("too small");
-    text = "too small"
-  } else {
-    alertBox.classList.add("success");
-    text = "SUCCESS";
-    // console.log("success");
-    let play = prompt("Would you like to play again? ")
-  if(play !== "yes"){
+async function guessANumber() {
+  let newPromise = new Promise((resolve, reject) => {
+    while (!endOfFile) {
+      // NOTE: NEED TO GENERATE A NEW NUMBER
+      yourNumber = Number(prompt("Please enter a number:  "));
+      if (yourNumber > computerGuess) {
+        console.log("too large");
+        text = "too large";
+        setTimeout(() => resolve("too large!"), 1000)
+         setTimeout(() => reject("too slow"), 1000)
+        // resolve("too large")
+        p.innerText = text;
+      } else if (yourNumber < computerGuess) {
+        console.log("too small");
+        text = "too small";
+        // resolve("too small")
+        setTimeout(() => resolve("too small"), 1000)
+        p.innerText = text;
+      } else {
+        alertBox.classList.add("success");
+        text = "SUCCESS";
+        // resolve("success")
+        // p.innerText = text;
+        // console.log("success");
+         setTimeout(() => resolve("too small"), 1000)
+        let play = prompt("Would you like to play again? ");
+        if (play !== "yes") {
+          break;
+        }
 
-    break
-  }
-  
-  continue
-  }
-  
-  continue;
+        continue;
+      }
+
+      continue;
+    }
+  });
+  let result = await newPromise;
+  p.innerText = result;
+  // consol?e.log(result);
 }
-}
 
-
-const myPromise = new Promise((resolve, reject)=>{
-  if(guessANumber){
-    resolve('There is a value')
-  }else{
-    reject("there is no number")
-  }
-})
-myPromise.then(text)
-
-
-
+// const myPromise = new Promise((resolve, reject)=>{
+//   if(guessANumber){
+//     resolve(p.innerText=text)
+//   }else{
+//     reject("there is no number")
+//   }
+// })
+// myPromise.then(console.log('Done'))
